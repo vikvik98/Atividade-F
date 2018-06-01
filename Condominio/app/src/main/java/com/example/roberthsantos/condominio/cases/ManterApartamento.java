2bc5fdeaf2bbbe8506f07cf0d26c42284057e241
RoberthBSantos <fco.roberth@gmail.com>
2018-06-01 00:08:25 -0300

    FixCases

--- Condominio/app/src/main/java/com/example/roberthsantos/condominio/cases/ManterApartamento.java
+++ Condominio/app/src/main/java/com/example/roberthsantos/condominio/cases/ManterApartamento.java
@@ -1,12 +1,13 @@
 package com.example.roberthsantos.condominio.cases;
 
 import com.example.roberthsantos.condominio.model.Apartamento;
+import com.example.roberthsantos.condominio.model.EnumTipoOcupacao;
 import com.example.roberthsantos.condominio.model.Proprietario;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ManterApartamento {
+public class ManterApartamento extends ManterProprietario{
 
     private List<Apartamento> apartamentos;
 
@@ -14,12 +15,20 @@ public class ManterApartamento {
         this.apartamentos = new ArrayList<>();
     }
 
-    public void CadastrarApartamento(int numero, int qtdQuartos){
-        //TODO: Tratamento para saber se tem proprietario ou nao
+    public boolean CadastrarApartamento(int numero, int qtdQuartos, EnumTipoOcupacao opDoEnum){
         Apartamento apartamento = new Apartamento();
-        apartamento.setNumeroDoAp(numero);
-        apartamento.setQtdQuartos(qtdQuartos);
-        apartamentos.add(apartamento);
+        for(int i = 0; i < apartamentos.size(); i++){
+            if(apartamentos.get(i).getNumeroDoAp() == numero){
+                return false;
+            }else {
+                apartamento.setNumeroDoAp(numero);
+                apartamento.setQtdQuartos(qtdQuartos);
+                apartamento.setOpDoEnum(opDoEnum);
+                apartamentos.add(apartamento);
+                return true;
+            }
+        }
+        return true;
     }
 
     public void AssociarProprietario(int numeroAp, String nomeProprietario){
@@ -30,10 +39,19 @@ public class ManterApartamento {
             }
         }
 
+        for(int i = 0; i < getProprietarios().size(); i++){
+            if(getProprietarios().get(i).getNome().equals(nomeProprietario)){
+                getProprietarios().get(i).getApartamentos().add(apartamento);
+            }
+        }
+
 
     }
 
-
+    @Override
+    public List<Proprietario> getProprietarios() {
+        return super.getProprietarios();
+    }
 
     public List<Apartamento> getApartamentos() {
         return apartamentos;
